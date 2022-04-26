afebe497f1e3b9d8a0579f8050258aa51b210b1a
agushery <hebatmandor@gmail.com>
2022-04-26 21:23:33 +0800

    Change Profile View

--- The Eye/Profile/ProfileView.swift
+++ The Eye/Profile/ProfileView.swift
@@ -10,47 +10,41 @@ import SwiftUI
 struct ProfileView: View {
     @State var name: String
     @State var phoneNumber: String
+    @State private var enableBlogger = true
     var body: some View {
-        NavigationView {
-            VStack{
-                Image("Profile")
-                    .frame(minWidth: 50, minHeight: 50)
+            NavigationView {
+                Form {
+                    Image("Profile")
+                    .resizable()
+                    .clipped()
+                    .frame(width: 100, height: 100, alignment: .center)
                     .clipShape(Circle())
-                    .shadow(radius: 10)
-                    .padding(.top,30)
-                    .padding(.bottom,50)
-                //
-                HStack(alignment: .center){
-                    Text("Name : ")
-                        .font(.callout)
-                        .bold()
-                        .frame(alignment: .leading)
-                    TextField("Enter username...", text: $name)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                }
-                .padding(.leading)
-                .padding(.trailing)
-                //
-                
-                HStack(alignment: .center){
-                    Text("Phone Number : ")
-                        .font(.callout)
-                        .bold()
-                        .frame(alignment: .leading)
-                    TextField("Number only...", text: $phoneNumber)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .keyboardType(.decimalPad)
-                }
-                .padding(.leading)
-                .padding(.trailing)
-                
-                //
-                Spacer()
-                
+                    .overlay(Circle().stroke(Color.blue, lineWidth: 2.0))
+                    Section(header: Text("Basic Information")) {
+                        HStack {
+                            Text("First Name")
+                            Spacer(minLength: 100)
+                            Text("Agus")
+                        }
+                        HStack {
+                            Text("Last Name")
+                            Spacer(minLength: 100)
+                            Text("Hery")
+                        }
+                    }
+                    Section(header: Text("Additional")) {
+                        HStack {
+                            Text("Gender")
+                            Spacer(minLength: 100)
+                            Text("Male")
+                        }
+                        Toggle(isOn: $enableBlogger) {
+                            Text("Blogger")
+                        }
+                    }
+                }.navigationBarTitle(Text("Profile"))
             }
-            .navigationTitle("Profile")
         }
-    }
 }
 
 struct ProfileView_Previews: PreviewProvider {
