9205e81f4bd29208eaa77c2b61657058c37cb73e
agushery <hebatmandor@gmail.com>
2022-05-10 21:14:36 +0800

    add title chart

--- The Eye/Views/Status/StatusView.swift
+++ The Eye/Views/Status/StatusView.swift
@@ -19,54 +19,41 @@ struct StatusView: View {
         }
         return data
     }
-    func Moving() -> [Double]{
+    func Moving() -> (data: [Double], title: String?){
         let data = getData()
         let windowSize: Int = 3
         var sum: Double = 0.0
         var movingAverage: Double = 0.0
         let numberSize: Int = 9
+        let chartTitle: String = "Empty Data"
         if data.count > 7 {
             var forecasting: [Double] = data.suffix(7)
             for i in 0...(numberSize-windowSize) {
                 for j in i..<i+windowSize{
                     sum += forecasting[j]
-//                    print("\(forecasting[j]), ")
+                    //                    print("\(forecasting[j]), ")
                 }
                 movingAverage = sum / Double(windowSize)
                 forecasting.append(movingAverage)
-//                print("Moving average: \(movingAverage)")
+                //                print("Moving average: \(movingAverage)")
             }
-//            print(forecasting)
-            return forecasting.suffix(7)
+            //            print(forecasting)
+            return (forecasting.suffix(7), nil)
         }
         else{
-            return [0,0,0,0,0,0,0]
+            return ([0,0,0,0,0,0,0], chartTitle)
         }
     }
     
-    
-    
     var body: some View {
+        let result = Moving()
         NavigationView {
             VStack(alignment: .center){
-                LineView(data: Moving())
-                        .padding(.leading)
-                        .padding(.trailing)
-                        .padding(.bottom)
-                VStack{
-                    Picker(selection: $selectedValue, label: Text("someText")){
-                        Text("Your Data").tag(0)
-                        Text("Simple Moving Average").tag(1)
-                    }.pickerStyle(SegmentedPickerStyle())
-                    if (selectedValue == 0){
-                        SubviewA()
-                    }else if(selectedValue == 1){
-                        SubviewB()
-                    }else{
-                        EmptyView()
-                    }
-                }
-                Spacer(minLength: 100)
+                LineView(data: result.data, title: result.title)
+                    .padding(.leading)
+                    .padding(.trailing)
+                    .padding(.bottom)
+                Text("Test")
             } // Vstak 1
             .fullScreenCover(isPresented: $didOnboarding, content: {
                 Onboarding(didOnboarding: $didOnboarding)
@@ -75,16 +62,6 @@ struct StatusView: View {
         } // navView
     } // var body
 } // struct
-struct SubviewA: View{
-    var body: some View{
-        Text("Subview A")
-    }
-}
-struct SubviewB: View{
-    var body: some View{
-        Text("Subview B")
-    }
-}
 
 
 struct StatusView_Previews: PreviewProvider {
